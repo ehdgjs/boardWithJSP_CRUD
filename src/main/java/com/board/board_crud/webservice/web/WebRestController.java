12961b0199e8aee834f79c023eb58945a56d2494
ehdgjs <50191292+ehdgjs@users.noreply.github.com>
2020-07-06 01:14:58 +0900

    게시글 등록 활성화

--- src/main/java/com/board/board_crud/webservice/web/WebRestController.java
+++ src/main/java/com/board/board_crud/webservice/web/WebRestController.java
@@ -3,20 +3,18 @@ package com.board.board_crud.webservice.web;
 import com.board.board_crud.webservice.domain.posts.PostsRepository;
 import com.board.board_crud.webservice.dto.posts.PostsSaveRequestDto;
 import com.board.board_crud.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
     private PostsRepository postsRepository;
     private PostsService postsService;
-
-    public WebRestController(PostsRepository postsRepository){
-        this.postsRepository = postsRepository;
-    }
 
     @GetMapping("/hello")
     public String hello(){
@@ -24,7 +22,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public long savePosts(@RequestBody PostsSaveRequestDto dto){
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
     }
 }
