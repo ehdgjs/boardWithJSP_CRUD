268c143e633e9e92be4f84b80bf1cffb54baeb88
ehdgjs <50191292+ehdgjs@users.noreply.github.com>
2020-07-13 22:22:03 +0900

    CR구현

--- src/main/java/com/board/board_crud/webservice/web/webController.java
+++ src/main/java/com/board/board_crud/webservice/web/webController.java
@@ -1,15 +1,20 @@
 package com.board.board_crud.webservice.web;
 
+import com.board.board_crud.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class webController {
 
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
